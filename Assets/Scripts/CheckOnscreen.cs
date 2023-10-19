4510900a2fbdd6e781632b491d564c23ae832359
LuckyDDBH <ddbh19@student.aau.dk>
2023-10-19 10:56:48 +0200

    EnemyGuide"final"

--- Assets/Scripts/CheckOnscreen.cs
+++ Assets/Scripts/CheckOnscreen.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class CheckOnscreen : MonoBehaviour
 {
     public GameObject[] targets;
-    public GameObject[] guides;
-    public List<GameObject> heads = new List<GameObject>();
+    private GameObject[] guides;
+    private List<GameObject> heads = new List<GameObject>();
     public List<float> distancesToEnemy = new List<float>();
     public float shortestDistance;
-    public GameObject playerCharacter;
+    private GameObject playerCharacter;
     private GameObject nearestTarget;
     public GameObject nearestTargetName;
-    float distance;
-
-    public Vector3 compassDirection = new Vector3(0, 0, 0);
-
+    private Camera cam;
+    private Vector3 compassDirection = new Vector3(0, 0, 0);
+    public Vector3 arrowTipScreenPos;
+    public float screenHeight;
+    public float screenWidth;
     // Start is called before the first frame update
     void Start()
     {
         targetLocationSetup();
+        cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -29,6 +31,7 @@ public class CheckOnscreen : MonoBehaviour
         locateNearestEnemy();
         pointToNearestEnemy();
         hideCompass();
+        checkScreenPosition();
     }
 
     void targetLocationSetup()
@@ -38,13 +41,11 @@ public class CheckOnscreen : MonoBehaviour
         playerCharacter = GameObject.FindWithTag("Player");
         foreach (GameObject e in targets)
         {
-            //print(e.transform.FindChild("head"));
             heads.Add(e.transform.Find("head").gameObject);
         }
         for (int i = 0; i < targets.Length; i++)
         {
             distancesToEnemy.Add(Vector3.Distance(playerCharacter.transform.position, heads[i].transform.position));
-            //whoAlive.Add(targets[i].GetComponent<EnemyHealth>().alive);
         }
         shortestDistance = 10;
     }
@@ -82,6 +83,7 @@ public class CheckOnscreen : MonoBehaviour
     void pointToNearestEnemy()
     {
         compassDirection = ((playerCharacter.transform.position + nearestTargetName.transform.position) * 0.5f);
+        compassDirection = ((playerCharacter.transform.position + compassDirection) * 0.5f);
         transform.position = compassDirection;
         transform.LookAt(nearestTargetName.transform.position);
     }
@@ -102,5 +104,11 @@ public class CheckOnscreen : MonoBehaviour
                 guides[i].GetComponent<Renderer>().enabled = false;
             }
         }
+    }
+    void checkScreenPosition()
+    {
+        arrowTipScreenPos = cam.WorldToScreenPoint(guides[0].transform.position);
+        screenHeight = Screen.height;
+        screenWidth = Screen.width;
     }
 }
