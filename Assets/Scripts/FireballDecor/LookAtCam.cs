580d4bd2973616d30721e9db5de842c4a439fa87
LuckyDDBH <ddbh19@student.aau.dk>
2023-11-13 12:25:13 +0100

    ParticlePrettier

--- Assets/Scripts/FireballDecor/LookAtCam.cs
+++ Assets/Scripts/FireballDecor/LookAtCam.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveUp : MonoBehaviour
+public class LookAtCam : MonoBehaviour
 {
-    public float speed; //Speed is assigned in unity
+    private Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up * speed * Time.deltaTime;
+        transform.LookAt(cam.transform, Vector3.up);
     }
 }
