0a6ec72d52bb330e9592ff69bb6c439dbe1166e7
Rrha20 <83447577+Rrha20@users.noreply.github.com>
2023-12-11 12:44:53 +0100

    All data sort'd
    This was annoying. Cancel OneDrive

---
+++ ML/MED7ML/FeatureCreater.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+
+#Features
+# Each MI attempt
+# Log enemies alive at the time
+# In a fight? (Take damage - then attack - until goblin deded)
+Partpd = pd.DataFrame
+
+
